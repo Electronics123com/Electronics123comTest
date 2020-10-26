d0cea79c1c89259b4fd4a6e4375883881436d45f
Zeno Nanon <zna@odoo.com>
2020-10-26 10:41:29 -0700

    [ADD] Migrated Modules

---
+++ picking_report_customization/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Picking Report Customization",
+    'summary': "Poc",
+    'description': """
+Picking Report Customization
+""",
+    "author": "Odoo S.A.",
+    'website': "https://www.odoo.com",
+    'category': 'Custom Development',
+    'version': '0.1',
+    'depends': ['sale_stock'],
+    'data': [
+        'views/report.xml',
+    ],
+}
