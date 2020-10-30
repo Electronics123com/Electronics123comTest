df890be7915b06416d7f9b9998d32733c80230f9
Zeno Nanon <zna@odoo.com>
2020-10-30 10:26:26 -0700

    [ADD] migrated modules

---
+++ website_sale_search/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "E-commerce Advanced Search",
+    'summary': "Search",
+    'description': """
+E-commerce Advanced Search
+""",
+    "author": "Odoo S.A.",
+    'website': "https://www.odoo.com",
+    'category': 'Custom Development',
+    'version': '0.1',
+    'depends': ['website_sale'],
+    'data': [
+        'views/website_sale_search_ec.xml',
+    ],
+}
